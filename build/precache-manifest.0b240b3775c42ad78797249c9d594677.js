666640c4a5df3bd18ca9bcab294f8c64a53051aa
Prabhat Tiwari <prabhattiwari0602@gmail.com>
2023-04-10 17:36:13 +0530

    build

---
+++ build/precache-manifest.0b240b3775c42ad78797249c9d594677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10773b8ba2f698b3f032ec03d2ffd8f9",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "14792d297bdd190eb6bc",
+    "url": "/react-gh-pages/static/css/main.10078b94.chunk.css"
+  },
+  {
+    "revision": "adf13ecee7562b676f86",
+    "url": "/react-gh-pages/static/js/2.af7af803.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/react-gh-pages/static/js/2.af7af803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14792d297bdd190eb6bc",
+    "url": "/react-gh-pages/static/js/main.125b7779.chunk.js"
+  },
+  {
+    "revision": "725e20e61e6c0e51b5fc",
+    "url": "/react-gh-pages/static/js/runtime-main.23b523d3.js"
+  }
+]);
